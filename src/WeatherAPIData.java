426910e904ee236da7cbea5fb1ed6fc116c9d711
aimbxtl <067alexl@gmail.com>
2024-11-08 18:27:24 -0700

    clean up

--- src/WeatherAPIData.java
+++ src/WeatherAPIData.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
